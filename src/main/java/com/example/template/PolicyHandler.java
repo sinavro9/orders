fd0f9fc4fdb8f3863c731ba4a9905cafd919432c
user15 <user15@naver.com>
2020-06-16 07:21:21 +0900

    Modified to PolicyHandler

--- src/main/java/com/example/template/PolicyHandler.java
+++ src/main/java/com/example/template/PolicyHandler.java
@@ -1,22 +1,13 @@
 package com.example.template;
 
 import com.example.template.config.kafka.KafkaProcessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.util.Optional;
-
 @Service
-public class OrderService {
+public class PolicyHandler {
 
     @Autowired
     private ProductRepository productRepository;
